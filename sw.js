9a0ebf520341a6b9f8678e82b9a317ef935cd702
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-01-27 13:17:37 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'taxi-promax-v1';
+const assets = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
