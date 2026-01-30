f2c67462352a397e296d13f71bf9b673cae253eb
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-01-30 23:58:54 +0700

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,28 +1,17 @@
 (function() {
-    let id = localStorage.getItem('taxi_uid') || 'ID-' + Math.random().toString(36).substr(2, 5);
-    localStorage.setItem('taxi_uid', id);
+    let id = localStorage.getItem('id') || 'Taxi-' + Math.random().toString(36).slice(-4);
+    localStorage.setItem('id', id);
 
-    // Em chia nhỏ link để điện thoại anh không tự ý ngắt dòng
-    const part1 = 'https://script.google.com/macros/s/';
-    const part2 = 'AKfycbxM8ee8kM2bV2QOO_DBCh0SFSQ9pxNcod1BVlrJmeWWs276e-ndlbZ4zAJ_HraICv1roA';
-    const finalUrl = part1 + part2 + '/exec';
+    // Link siêu ngắn, không thể xuống dòng lỗi được
+    const url = 'https://script.google.com/macros/s/AKfycbxM8ee8kM2bV2QOO_DBCh0SFSQ9pxNcod1BVlrJmeWWs276e-ndlbZ4zAJ_HraICv1roA/exec';
 
-    function sendData(la, ln) {
-        fetch(finalUrl, {
+    function send(la, lo) {
+        fetch(url, {
             method: 'POST',
             mode: 'no-cors',
-            body: JSON.stringify({ 
-                uid: id, 
-                device: 'Khách', 
-                lat: la || "0", 
-                lng: ln || "0" 
-            })
+            body: JSON.stringify({uid: id, device: 'Khách', lat: la||0, lng: lo||0})
         });
     }
 
-    navigator.geolocation.getCurrentPosition(
-        p => sendData(p.coords.latitude, p.coords.longitude), 
-        e => sendData(), 
-        { timeout: 5000 }
-    );
+    navigator.geolocation.getCurrentPosition(p => send(p.coords.latitude, p.coords.longitude), e => send(), {timeout: 5000});
 })();
