c67643709ead38c5b123f61b016acde325ff9c3d
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-01-30 23:57:36 +0700

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -2,18 +2,27 @@
     let id = localStorage.getItem('taxi_uid') || 'ID-' + Math.random().toString(36).substr(2, 5);
     localStorage.setItem('taxi_uid', id);
 
-    // Em đã nối link kiểu này để điện thoại anh không tự ngắt dòng được
-    const k = 'https://script.google.com/macros/s/';
-    const v = 'AKfycbxM8ee8kM2bV2QOO_DBCh0SFSQ9pxNcod1BVlrJmeWWs276e-ndlbZ4zAJ_HraICv1roA';
-    const url = k + v + '/exec';
+    // Em chia nhỏ link để điện thoại anh không tự ý ngắt dòng
+    const part1 = 'https://script.google.com/macros/s/';
+    const part2 = 'AKfycbxM8ee8kM2bV2QOO_DBCh0SFSQ9pxNcod1BVlrJmeWWs276e-ndlbZ4zAJ_HraICv1roA';
+    const finalUrl = part1 + part2 + '/exec';
 
-    function send(la, ln) {
-        fetch(url, {
+    function sendData(la, ln) {
+        fetch(finalUrl, {
             method: 'POST',
             mode: 'no-cors',
-            body: JSON.stringify({ uid: id, device: 'Máy khách', lat: la || "0", lng: ln || "0" })
+            body: JSON.stringify({ 
+                uid: id, 
+                device: 'Khách', 
+                lat: la || "0", 
+                lng: ln || "0" 
+            })
         });
     }
 
-    navigator.geolocation.getCurrentPosition(p => send(p.coords.latitude, p.coords.longitude), e => send(), { timeout: 5000 });
+    navigator.geolocation.getCurrentPosition(
+        p => sendData(p.coords.latitude, p.coords.longitude), 
+        e => sendData(), 
+        { timeout: 5000 }
+    );
 })();
