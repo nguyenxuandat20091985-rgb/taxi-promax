db276cde202df74facaece92982e3d3e465c47fc
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-01-31 08:35:59 +0700

    Update ai-config.js

--- ai-module/ai-config.js
+++ ai-module/ai-config.js
@@ -4,7 +4,7 @@ const AI_CONFIG = {
     apiEndpoint: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent",
     
     // Đã dán mã thật của anh Đạt vào đây
-    apiKey: "AIzaSyAxvauzbDU86FWsgDOX2zo7uii2sxaW3_E", 
+    apiKey: "AIzaSyBYIpmslXFTkETW7cfiPeLJ0oPcgMJUn2g", 
 
     welcomeMessage: "TAXI AI PROMAX xin chào!"
 };
