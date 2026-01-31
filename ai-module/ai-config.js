3eb821549fd69179e29688852e04b49ccd923b5a
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-01-31 08:06:09 +0700

    Update ai-config.js

--- ai-module/ai-config.js
+++ ai-module/ai-config.js
@@ -6,5 +6,5 @@ const AI_CONFIG = {
     // Mã API Key của anh đã được kích hoạt tại đây
     apiKey: "AIzaSyAxvauzbDU86FWsgDOX2zo7uii2sxaW3_E", 
 
-    welcomeMessage: "Chào đại ca ! Trợ lý TAXI PROMAX đã sẵn sàng. Hôm nay anh muốn em hỗ trợ gì cho chuyến xe của mình không?"
+    welcomeMessage: "TAXI PROMAX xin chào!?"
 };
